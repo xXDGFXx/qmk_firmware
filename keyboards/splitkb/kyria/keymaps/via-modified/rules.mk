3cd23afc60cad57b2a777ef07725948b26f8c208
Zenith Auduong <zenith.auduong@entronix.io>
2023-09-22 16:58:27 -0500

    working on kyria layout

---
+++ keyboards/splitkb/kyria/keymaps/via-modified/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+NKRO_ENABLE = yes
+WPM_ENABLE = yes
+
+KEY_LOCK_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+GRAVE_ESCAPE_ENABLE = no
+
+LTO_ENABLE = no
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
