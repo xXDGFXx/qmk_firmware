0d911026411152deb7ba6e729ee3df07db56195c
Zenith Auduong <zenith.auduong@entronix.io>
2024-03-21 15:18:46 -0500

    Committing recent changes

--- keyboards/controllerworks/mini42/keymaps/via-modified/config.h
+++ keyboards/controllerworks/mini42/keymaps/via-modified/config.h
@@ -15,8 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define TAPPING_TOGGLE 2
 
 /*
  * Mod-tap adjustments
